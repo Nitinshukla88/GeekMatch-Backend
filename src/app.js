095e70f64b51130a74ed7ed4e67994f8fe9d65ed
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-14 14:31:54 +0530

    learned about regex passing in routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user/:userId/:city", (req, res)=> { // Here in user route, userId, city are dynamic parameters
+    console.log(req.params); // To get parameters from route, you can use params method
+    res.send({firstname : "Nitin", lastname : "Shukla"});
+})
+
+app.post("/user", (req, res)=> {
+    res.send("Data is successfully stored to DB");
+})
+
 app.use("/test", (req, res) => {
     res.send("This is the test page!");
 })
