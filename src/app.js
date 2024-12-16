bfa756e7402a0c535df6ed57db7af7d01e5ed189
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-16 15:22:32 +0530

    learned dynamic data fetch from request url and parsing using express middleware

--- src/app.js
+++ src/app.js
@@ -4,11 +4,38 @@ const app = express();
 
 const connectDB = require("./config/database");
 
-connectDB().then(()=> {
+const User = require("./models/user");
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    console.log(req.body);
+//   const userData = {
+//     firstName: "raj",
+//     lastName: "shrivastava",
+//     password: 163535,
+//     age: 13,
+//   };
+
+//   const user = new User(userData);
+
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.send("Data saved successfully!!");
+  } catch (err) {
+    res.send("Error in saving the user- " + err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
     console.log("Database is successfully connected");
-    app.listen(4000, ()=> {
-        console.log("Server is listening on port 4000");
-    })
-}).catch((err)=> {
+    app.listen(4000, () => {
+      console.log("Server is listening on port 4000");
+    });
+  })
+  .catch((err) => {
     console.error("Database is not connected !!");
-})
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=> {
-    await mongoose.connect("mongodb+srv://nitinshukla12004:QnHTjbVGORRrlJVC@nodejslearning.ldcg1.mongodb.net/");
+    await mongoose.connect("mongodb+srv://nitinshukla12004:QnHTjbVGORRrlJVC@nodejslearning.ldcg1.mongodb.net/GeekMatch");
 }
 
 module.exports = connectDB;
