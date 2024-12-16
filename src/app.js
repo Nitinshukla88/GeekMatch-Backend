ec476e7e358cde56b87c3804288b43f7c14d5413
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-16 21:08:44 +0530

    wrote the patch method to update the database document

--- src/app.js
+++ src/app.js
@@ -9,15 +9,15 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    console.log(req.body);
-//   const userData = {
-//     firstName: "raj",
-//     lastName: "shrivastava",
-//     password: 163535,
-//     age: 13,
-//   };
+  console.log(req.body);
+  //   const userData = {
+  //     firstName: "raj",
+  //     lastName: "shrivastava",
+  //     password: 163535,
+  //     age: 13,
+  //   };
 
-//   const user = new User(userData);
+  //   const user = new User(userData);
 
   const user = new User(req.body);
 
@@ -28,6 +28,55 @@ app.post("/signup", async (req, res) => {
     res.send("Error in saving the user- " + err.message);
   }
 });
+
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const user = await User.findOne({ email: userEmailId });
+    if (user.length === 0) {
+      res.status(401).send("User does not exist");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
+
+app.delete("/user", async(req, res) => {
+  const userId = req.body.id;
+  try{
+    // const user = await User.findOneAndDelete({ _id : userId }); This and the below method is one and the same
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }catch(err){
+    res.status(401).send("Something went wrong!!");
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("Something went wrong");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(401).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async(req, res) => {
+  const userId = req.body.id;
+  const data = req.body
+  try{
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User is updated successfully!");
+  }catch(err){
+    res.send("Something went Wrong- "+err.message);
+  }
+})
 
 connectDB()
   .then(() => {
