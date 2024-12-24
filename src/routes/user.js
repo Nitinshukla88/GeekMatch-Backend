116161a9d5c22829e88f4bd3fd7f62f96c788927
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-24 10:44:46 +0530

    wrote the connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,13 +6,37 @@ const { userAuth } = require("../middlewares/auth");
 
 const ConnectionRequest = require("../models/connectionrequest");
 
+const USER_DATA_FIELDS = ["firstName", "lastName", "skills"];
+
 userRouter.get("/user/requests/received", userAuth, async(req, res) => {
     try{
 
         const loggedInUser = req.user;
-        const matchedRequests = await ConnectionRequest.find({ toUserId : loggedInUser?._id, status : "interested" });
+        const matchedRequests = await ConnectionRequest.find({ toUserId : loggedInUser?._id, status : "interested" }).populate("fromUserId", ["firstName", "lastName"]);
         res.json({ message : "Requests fetched successfully", matchedRequests });
         
+    }catch(err){
+        res.status(401).send("Error: " + err.message);
+    }
+})
+
+
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const totalConnections = await ConnectionRequest.find({ $or : [{ fromUserId : loggedInUser._id, status : "accepted"}, { toUserId : loggedInUser._id, status : "accepted" }]}).populate("fromUserId", USER_DATA_FIELDS).populate("toUserId", USER_DATA_FIELDS);
+
+
+        const data = totalConnections.map(connection => {
+            if(connection?.fromUserId._id.toString() === loggedInUser?._id.toString()){
+                return connection?.toUserId;
+            }
+            return connection?.fromUserId;
+        })
+
+        res.json({ message : "Connections fetched Successfully", data : data });
+
     }catch(err){
         res.status(401).send("Error: " + err.message);
     }
