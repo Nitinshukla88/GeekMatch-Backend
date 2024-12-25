f83ba32175d24c514a926d570c0b087d5b9cd0e8
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-25 13:30:56 +0530

    wrote the feed API successfully

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,7 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
 const ConnectionRequest = require("../models/connectionrequest");
+const User = require("../models/user");
 
 const USER_DATA_FIELDS = ["firstName", "lastName", "skills"];
 
@@ -39,6 +40,29 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
 
     }catch(err){
         res.status(401).send("Error: " + err.message);
+    }
+})
+
+userRouter.get("/user/feed", userAuth , async(req, res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const totalConnections = await ConnectionRequest.find({ $or : [{fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}]}).select("fromUserId toUserId");
+
+        const usersToHideFromFeed = new Set();
+
+        totalConnections.forEach((req) => {
+            usersToHideFromFeed.add(req?.fromUserId.toString());
+            usersToHideFromFeed.add(req?.toUserId.toString());
+        })
+
+        const usersToShow = await User.find({ $and : [{_id : { $nin : Array.from(usersToHideFromFeed) }}, { _id : { $ne : loggedInUser._id }}] }).select(USER_DATA_FIELDS)  // Array.from(<Set>) converts the set into array
+
+        res.json({ data : usersToShow });
+
+
+    }catch(err){
+        res.status(404).json({message : err.message});
     }
 })
 
