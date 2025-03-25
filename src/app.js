52198f5a2e2e4430e13846a4db32cc41e5f7562c
nitinshukla <nitin.shukla12004@gmail.com>
2025-03-25 16:37:00 +0530

    added the vercel deployed frontend url

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const cors = require("cors");
 const initializeSocket = require("./utils/socket");
 
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : "https://geek-match-frontend.vercel.app",
   credentials : true
 }));
 app.use(express.json());
